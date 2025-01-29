5ac48d5e29f2f4ef2f182c13069afac01130d5c5
Marcelo Costa <marcelocosta@facephi.com>
2025-01-29 15:20:23 +0100

    adding small changes [skip ci]

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,19 @@
-import typescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
-import { Linter } from "eslint";
-import playwright from "eslint-plugin-playwright";
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import { Linter } from 'eslint';
+import playwright from 'eslint-plugin-playwright';
 
 interface ESLintRules extends Linter.RulesRecord {
-  "playwright/no-wait-for-timeout": Linter.RuleEntry;
-  "playwright/prefer-web-first-assertions": Linter.RuleEntry;
-  "playwright/quotes": ["error" | "warn" | "off", "single" | "double"];
+  'playwright/no-wait-for-timeout': Linter.RuleEntry;
+  'playwright/prefer-web-first-assertions': Linter.RuleEntry;
+  'playwright/quotes': ['error' | 'warn' | 'off', 'single' | 'double'];
   quotes: [
-    "error" | "warn" | "off",
-    "single" | "double",
+    'error' | 'warn' | 'off',
+    'single' | 'double',
     { avoidEscape?: boolean }?,
   ];
-  "@typescript-eslint/no-explicit-any": Linter.RuleEntry;
-  "@typescript-eslint/explicit-function-return-type": Linter.RuleEntry;
+  '@typescript-eslint/no-explicit-any': Linter.RuleEntry;
+  '@typescript-eslint/explicit-function-return-type': Linter.RuleEntry;
 }
 
 interface ESLintConfig {
@@ -22,10 +22,10 @@ interface ESLintConfig {
   languageOptions?: {
     parser: typeof typescriptParser;
     ecmaVersion?: number;
-    sourceType?: "module" | "script";
+    sourceType?: 'module' | 'script';
   };
   plugins?: {
-    "@typescript-eslint": typeof typescript;
+    '@typescript-eslint': typeof typescript;
     playwright: typeof playwright;
   };
   rules?: Partial<ESLintRules>;
@@ -33,32 +33,32 @@ interface ESLintConfig {
 
 const config: ESLintConfig[] = [
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["node_modules/**", "dist/**"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules/**', 'dist/**'],
     languageOptions: {
       parser: typescriptParser,
       ecmaVersion: 2020,
-      sourceType: "module",
+      sourceType: 'module',
     },
     plugins: {
-      "@typescript-eslint": typescript,
+      '@typescript-eslint': typescript,
       playwright,
     },
     rules: {
       ...typescript.configs.recommended.rules,
       ...playwright.configs.recommended.rules,
-      "playwright/no-wait-for-timeout": "warn",
-      "playwright/prefer-web-first-assertions": "warn",
-      quotes: ["error", "single"],
-      "playwright/quotes": ["error", "single"],
+      'playwright/no-wait-for-timeout': 'warn',
+      'playwright/prefer-web-first-assertions': 'warn',
+      quotes: ['error', 'single'],
+      'playwright/quotes': ['error', 'single'],
     },
   },
   {
-    files: ["tests/**/*.{js,jsx,ts,tsx}"],
+    files: ['tests/**.{js,jsx,ts,tsx}'],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      quotes: ["error", "single", { avoidEscape: true }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      quotes: ['error', 'single', { avoidEscape: true }],
     },
   },
 ];
