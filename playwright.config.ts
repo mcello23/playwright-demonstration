5e0a3879a4e62bd1bf9fa6954d012c5c4334e7a0
Marcelo Costa <marcelocosta@facephi.com>
2025-01-29 10:06:56 +0100

    updating dotenv params

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,19 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
-// Read from ".env" file.
 dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-// Alternatively, read from "../my.env" file.
-dotenv.config({ path: path.resolve(__dirname, '..', 'my.env') });
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
