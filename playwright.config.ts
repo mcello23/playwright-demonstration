8dcf18360cf43c0b63b73cdff5d23adbf4373fa1
Marcelo Costa <marcelocosta@facephi.com>
2025-02-03 17:06:13 +0100

    updating paralel

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 0,
-  workers: process.env.CI ? 3 : 4,
+  workers: process.env.CI ? 2 : 4,
   reporter: [
     [
       'allure-playwright',
