56f1990bb32c385eb2015a242711474ce80e290d
Marcelo Costa <marcelocosta@facephi.com>
2025-02-20 17:14:02 +0100

    adjusting tests on parallel/CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 1,
-  workers: process.env.CI ? 2 : 4,
+  retries: process.env.CI ? 3 : 1,
+  workers: process.env.CI ? 3 : 4,
   reporter: [
     ['list'],
     ['html'],
