9139a382c839006e991cc25ec7e2910345b2115d
Marcelo Costa <marcelocosta@facephi.com>
2025-03-12 14:20:24 +0100

    updating timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
-  timeout: process.env.CI ? 45_000 : 30_000,
-  expect: { timeout: process.env.CI ? 30_000 : 15_000 },
+  timeout: process.env.CI ? 55_000 : 30_000,
+  expect: { timeout: process.env.CI ? 40_000 : 15_000 },
   testDir: './tests',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
